1b44901c6611876b82fed7b6d049c2f4923e0492
Leonzera <leonandrade22@gmail.com>
2022-07-05 17:17:59 -0300

    Some updates

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Lara</b>Dash',
+    'logo' => '<b>M3</b>Taskmaster',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
